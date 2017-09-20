ebf35b86b3d243a266e920df096403e7f87e1104
Pep Mendez <jmendez1@xtec.cat>
2017-09-20 20:08:25 +0200

    Events initials on button click

--- app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
+++ app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package jmendez.escoladeltreball.org.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     /*
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn_clickeOnClick(View view) {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show();
     }
 }
